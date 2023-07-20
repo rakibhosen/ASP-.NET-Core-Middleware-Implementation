5f1161814b44df02e2450a6b85d0096fa0c67cab
rakibhosen <rakibinfo832@gmail.com>
2023-07-20 15:56:19 +0600

    Initial Commit

---
+++ Middleware/RequestLoggingMiddlewarecs.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace REQUESTLOGGER.Middleware
+{
+
+    public class RequestLoggingMiddleware : IMiddleware
+    {
+        private readonly ILogger<RequestLoggingMiddleware> _logger;
+
+        public RequestLoggingMiddleware(ILogger<RequestLoggingMiddleware> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            // Log the incoming request method and path
+            _logger.LogInformation($"Request Method: {context.Request.Method}, Path: {context.Request.Path}");
+
+            // Call the next middleware in the pipeline
+            await next(context);
+        }
+    }
+}
